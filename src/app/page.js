37f6a1f1c9184786c65c9640613ea2a33950fd5a
LipeAKugel <felipearaujodff@gmail.com>
2023-12-25 18:16:07 -0300

    :bulb: styling home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,20 @@
+import styles from "../styles/app/home.module.css"
 
 export default function Home() {
   return (
-    <div className="home-container">
-      <h1>Home page</h1>
+    <div className={styles.container}>
+
+      <div className="card" id={styles.contentcard}>
+        <div className={styles.leftcontent}>
+          <h1>Hi,</h1>
+          <h1>I am Felipe Amorim</h1>
+          <h1>Software Developer</h1>
+        </div>
+        <div className={styles.rightcontent}>
+
+        </div>
+      </div>
+
     </div>
   )
 }
