0136db48f214a152a72447d0d80bfb09a99b9c36
LipeAKugel <felipearaujodff@gmail.com>
2023-12-27 03:05:40 -0300

    :bulb: adding about us page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,31 @@
+import styles from "../../styles/app/about.module.css"
+
 export default function AboutMe() {
   return (
-    <h1>About me page</h1>
+    <div className={styles.container}>
+
+      <div className="card" id={styles.contentcard}>
+        <h1>About Me</h1>
+        <p>
+          Hello, I’m a Software Engineering student at 
+          Universidade de Brasília (FGA campus). I’m 
+          currently in my 5th semestrer and trying to 
+          participate in new projects and learn new 
+          technologies. My passion for software comes 
+          from the experience of always leaning 
+          something new, I take care of the architecture 
+          and code quality of everything I build.
+        </p>
+        <h1>Experience</h1>
+        <p>
+          I have experience in full-stack development 
+          through college and personal projects. I’m also 
+          a game development enthusiast. Currently 
+          working on personal projects, looking to learn 
+          and experience new stuff and technologies.
+        </p>
+      </div>
+
+    </div>
   )
 }
